b02a8ecb1153f506991b930e7924558f4024b936
Andre Mapa <andreomapa01@gamil.com>
2022-03-15 16:42:05 -0300

    Movie card

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,8 +1,30 @@
-import Pagination from "components/Navbar/Pagination";
+import MovieCard from "components/MovieCard";
+import Pagination from "components/Pagination";
 
-function Listing(){
-    return(
-        <Pagination />
+function Listing() {
+    return (
+        <>
+            <Pagination />
+            <div className="container">
+                <div className="row">
+                    <div className="col-sm-6 col-md-4 col-lg-3 mb-3">
+                        <MovieCard />
+                    </div>
+                    <div className="col-sm-6 col-md-4 col-lg-3 mb-3">
+                        <MovieCard />
+                    </div>
+                    <div className="col-sm-6 col-md-4 col-lg-3 mb-3">
+                        <MovieCard />
+                    </div>
+                    <div className="col-sm-6 col-md-4 col-lg-3 mb-3">
+                        <MovieCard />
+                    </div>
+                    <div className="col-sm-6 col-md-4 col-lg-3 mb-3">
+                        <MovieCard />
+                    </div>
+                </div>
+            </div>
+        </>
     );
 }
 
